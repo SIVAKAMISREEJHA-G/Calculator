769f166476e96b341028568f764d125fa5af0a28
SIVAKAMISREEJHA-G <149347126+SIVAKAMISREEJHA-G@users.noreply.github.com>
2024-02-28 21:17:03 +0530

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,13 @@
+let output = document.getElementById("output");
+
+function myStopFunction() {
+    try {
+        let result = eval(output.value);
+        if (isNaN(result)) {
+            throw new Error("INVALID");
+        }
+        output.value = result;
+    } catch (err) {
+        alert(err.message);
+    }
+}
